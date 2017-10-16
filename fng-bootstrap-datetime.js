dedf3c97272643121833765e9ea3004cf73b401e
Mark Chapman <mark.chapman@gmail.com>
2017-10-17 00:09:15 +0100

    Set the form to $dirty on change

--- fng-bootstrap-datetime.js
+++ fng-bootstrap-datetime.js
@@ -1,11 +1,15 @@
 (function () {
   'use strict';
   var uiBootstrapDateModule = angular.module('fng.uiBootstrapDateTime', ['ui.bootstrap']);
-  uiBootstrapDateModule.directive('fngUiBootstrapDatetimePicker', ['$compile', '$filter', 'pluginHelper', 'formMarkupHelper', 'cssFrameworkService',
+  uiBootstrapDateModule.controller('fngUiBootstrapDatetimePickerCtrl',['$scope', function($scope) {
+      $scope.dateOptions = {};
+    }])
+    .directive('fngUiBootstrapDatetimePicker', ['$compile', '$filter', 'pluginHelper', 'formMarkupHelper', 'cssFrameworkService',
     function ($compile, $filter, pluginHelper) {
       return {
         restrict: 'E',
         replace: true,
+        controller: 'fngUiBootstrapDatetimePickerCtrl',
         priority: 1,
         link: function (scope, element, attrs) {
           var template;
@@ -15,24 +19,25 @@
             'show-meridian': false,
             'date-format': 'dd/MM/yyyy'
           };
-          overRiddenDefaults = Object.assign({}, overRiddenDefaults, processedAttr.directiveOptions);
-          var overRiddenDateStr = '';
-          if (processedAttr.directiveOptions['date-options']) {
-            var jsonDateOptions = JSON.parse(processedAttr.directiveOptions['date-options'].replace(/'/g, '"'));
-            if (scope.dateOptions) {
-              _.merge(scope.dateOptions, jsonDateOptions);
-              if (scope.dateOptions.showWeeks === undefined) {
-                scope.dateOptions.showWeeks = false;
-              }
-              overRiddenDateStr = ' date-options="dateOptions"'
-            } else {
-              var overRiddenDateDefaults = {
-                showWeeks: false
-              };
-              _.merge(overRiddenDateDefaults, jsonDateOptions);
-              overRiddenDateStr = ' date-options="' + JSON.stringify(overRiddenDateDefaults).replace(/"/g,"'") + '"'
+
+          // Set form to dirty when required.  Works OK with existing records - hopefully new records will have other field types
+          var watchField = attrs.model + '.' + attrs.fngFldName;
+          var formName = attrs.fngOptName;
+          scope.$watch(watchField, function (newVal, oldVal) {
+            if (newVal && oldVal && newVal !== oldVal) {
+              scope[formName].$setDirty();
             }
+          });
+
+          overRiddenDefaults = Object.assign({}, overRiddenDefaults, processedAttr.directiveOptions);
+          var overRiddenDateDefaults = {
+            showWeeks: false
+          };
+          var jsonDateOptions = {};
+          if (processedAttr.directiveOptions['date-options']) {
+            jsonDateOptions = JSON.parse(processedAttr.directiveOptions['date-options'].replace(/'/g, '"'));
           }
+          scope.dateOptions = Object.assign({}, overRiddenDateDefaults, jsonDateOptions);
           template = pluginHelper.buildInputMarkup(scope, attrs.model, processedAttr.info, processedAttr.options, false, false, function (buildingBlocks) {
             var str = '<div class="dtwrap"><datetimepicker ' + buildingBlocks.common;
             for (var opt in overRiddenDefaults) {
@@ -40,7 +45,7 @@
                 str += ' ' + opt + '="' + overRiddenDefaults[opt] + '"';
               }
             }
-            str += overRiddenDateStr + '></datetimepicker></div>';
+            str += ' date-options="dateOptions"></datetimepicker></div>';
             return str;
           });
           element.replaceWith($compile(template)(scope));
